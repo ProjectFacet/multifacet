e43777c5dfcf22de82eeee97186b529afcfa2976
Heather <heather@rebelcompass.com>
2020-04-06 04:02:44 -0700

    quick fixes

--- facet/editorial/views/views.py
+++ facet/editorial/views/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("It's a whole new gd ballgame.")
